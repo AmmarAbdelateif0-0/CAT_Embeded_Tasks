1a5df78071b9b453ff84cea873fafa90bba16748
Ammar Abdellateif <ammarabdelateif23@gmail.com>
2024-03-01 22:41:25 +0200

    modified

--- AVR/DIO/DIO_Config.h
+++ AVR/DIO/DIO_Config.h
@@ -11,88 +11,88 @@
 /******* define pin As input | output *******/
 /*********** PORTA_DIR *********/
 
-#define PA_Pin0_Dir
-#define PA_Pin1_Dir
-#define PA_Pin2_Dir
-#define PA_Pin3_Dir
-#define PA_Pin4_Dir
-#define PA_Pin5_Dir
-#define PA_Pin6_Dir
-#define PA_Pin7_Dir
+#define PA_Pin0_Dir     OUTPUT
+#define PA_Pin1_Dir     OUTPUT
+#define PA_Pin2_Dir     OUTPUT
+#define PA_Pin3_Dir     OUTPUT
+#define PA_Pin4_Dir     OUTPUT
+#define PA_Pin5_Dir     OUTPUT
+#define PA_Pin6_Dir     OUTPUT
+#define PA_Pin7_Dir     OUTPUT
 /*********** PORTB_DIR *********/
 
-#define PB_Pin0_Dir
-#define PB_Pin1_Dir
-#define PB_Pin2_Dir
-#define PB_Pin3_Dir
-#define PB_Pin4_Dir
-#define PB_Pin5_Dir
-#define PB_Pin6_Dir
-#define PB_Pin7_Dir
+#define PB_Pin0_Dir     OUTPUT
+#define PB_Pin1_Dir		OUTPUT
+#define PB_Pin2_Dir		OUTPUT
+#define PB_Pin3_Dir     OUTPUT
+#define PB_Pin4_Dir     OUTPUT
+#define PB_Pin5_Dir     OUTPUT
+#define PB_Pin6_Dir     OUTPUT
+#define PB_Pin7_Dir     OUTPUT
 /*********** PORTC_DIR *********/
 
-#define PC_Pin0_Dir
-#define PC_Pin1_Dir
-#define PC_Pin2_Dir
-#define PC_Pin3_Dir
-#define PC_Pin4_Dir
-#define PC_Pin5_Dir
-#define PC_Pin6_Dir
-#define PC_Pin7_Dir
+#define PC_Pin0_Dir     INPUT
+#define PC_Pin1_Dir     INPUT
+#define PC_Pin2_Dir     INPUT
+#define PC_Pin3_Dir     INPUT
+#define PC_Pin4_Dir     INPUT
+#define PC_Pin5_Dir     INPUT
+#define PC_Pin6_Dir     INPUT
+#define PC_Pin7_Dir     INPUT
 /*********** PORTD_DIR *********/
 
-#define PD_Pin0_Dir
-#define PD_Pin1_Dir
-#define PD_Pin2_Dir
-#define PD_Pin3_Dir
-#define PD_Pin4_Dir
-#define PD_Pin5_Dir
-#define PD_Pin6_Dir
-#define PD_Pin7_Dir
+#define PD_Pin0_Dir     INPUT
+#define PD_Pin1_Dir     INPUT
+#define PD_Pin2_Dir     INPUT
+#define PD_Pin3_Dir     INPUT
+#define PD_Pin4_Dir     INPUT
+#define PD_Pin5_Dir     INPUT
+#define PD_Pin6_Dir     INPUT
+#define PD_Pin7_Dir     INPUT
 /***********************************************/
 
 /******* define pin As HIGH |  LOW | FLOAT | PULL_UP *******/
 
 /*********** PORTA_VAL *********/
 
-#define PA_Pin0_VAL
-#define PA_Pin1_VAL
-#define PA_Pin2_VAL
-#define PA_Pin3_VAL
-#define PA_Pin4_VAL
-#define PA_Pin5_VAL
-#define PA_Pin6_VAL
-#define PA_Pin7_VAL
+#define PA_Pin0_VAL     HIGH
+#define PA_Pin1_VAL     HIGH
+#define PA_Pin2_VAL     HIGH
+#define PA_Pin3_VAL     HIGH
+#define PA_Pin4_VAL     HIGH
+#define PA_Pin5_VAL     HIGH
+#define PA_Pin6_VAL     HIGH
+#define PA_Pin7_VAL     HIGH
 /*********** PORTB_VAL *********/
 
-#define PB_Pin0_VAL
-#define PB_Pin1_VAL
-#define PB_Pin2_VAL
-#define PB_Pin3_VAL
-#define PB_Pin4_VAL
-#define PB_Pin5_VAL
-#define PB_Pin6_VAL
-#define PB_Pin7_VAL
+#define PB_Pin0_VAL    LOW
+#define PB_Pin1_VAL    LOW
+#define PB_Pin2_VAL    LOW
+#define PB_Pin3_VAL    LOW
+#define PB_Pin4_VAL    LOW
+#define PB_Pin5_VAL    LOW
+#define PB_Pin6_VAL    LOW
+#define PB_Pin7_VAL    LOW
 /*********** PORTC_VAL *********/
 
-#define PC_Pin0_VAL
-#define PC_Pin1_VAL
-#define PC_Pin2_VAL
-#define PC_Pin3_VAL
-#define PC_Pin4_VAL
-#define PC_Pin5_VAL
-#define PC_Pin6_VAL
-#define PC_Pin7_VAL
+#define PC_Pin0_VAL     FLOAT
+#define PC_Pin1_VAL     FLOAT
+#define PC_Pin2_VAL     FLOAT
+#define PC_Pin3_VAL     FLOAT
+#define PC_Pin4_VAL     FLOAT
+#define PC_Pin5_VAL     FLOAT
+#define PC_Pin6_VAL     FLOAT
+#define PC_Pin7_VAL     FLOAT
 /*********** PORTD_VAL *********/
 
-#define PD_Pin0_VAL
-#define PD_Pin1_VAL
-#define PD_Pin2_VAL
-#define PD_Pin3_VAL
-#define PD_Pin4_VAL
-#define PD_Pin5_VAL
-#define PD_Pin6_VAL
-#define PD_Pin7_VAL
+#define PD_Pin0_VAL     PULL_UP
+#define PD_Pin1_VAL     PULL_UP
+#define PD_Pin2_VAL     PULL_UP
+#define PD_Pin3_VAL     PULL_UP
+#define PD_Pin4_VAL     PULL_UP
+#define PD_Pin5_VAL     PULL_UP
+#define PD_Pin6_VAL     PULL_UP
+#define PD_Pin7_VAL     PULL_UP
 
 
 #endif /* DIO_CONFIG_H_ */
